11eb12bbae7b0ec77974669aca80e7b2b60acf29
Basil Wabuke <babazwabuke4@gmail.com>
2025-02-07 00:18:32 +0300

    changes to config.py file

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///cleaners.db') 
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///cleaners.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')  
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret-key')  
